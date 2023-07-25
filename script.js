1265c84beb6d0f35cc5f7d4e6bb9a95617b0102a
Aniket <aniketng08@gmail.com>
2023-07-25 22:41:11 +0530

    All work done.

--- script.js
+++ script.js
@@ -12,19 +12,21 @@ let periodA = false;
 let periodB = false;
 
 function add(a, b) {
-  return (a + b).toFixed(2);
+  return a + b;
 }
 
 function sub(a, b) {
-  return (a - b).toFixed(2);
+  return a - b;
 }
 function mul(a, b) {
-  return (a * b).toFixed(2);
+  return a * b;
 }
 function div(a, b) {
-  return Math.round((a * 100.0) / b) / 100;
+  if (b === 0) return "ERR";
+  return a / b;
 }
 function mod(a, b) {
+  if (b === 0) return "ERR";
   return a % b;
 }
 
@@ -53,7 +55,10 @@ function processButtonClick(e) {
         if (btnText === "0") return;
         opA = btnText;
         if (!minusA) processingText.textContent = btnText;
-        else processingText.textContent += btnText;
+        else {
+          processingText.textContent += btnText;
+          opA = (opA * -1).toString();
+        }
       } else {
         opA += btnText;
         processingText.textContent += btnText;
@@ -61,6 +66,7 @@ function processButtonClick(e) {
     } else {
       if (opB === undefined) {
         opB = btnText;
+        if (minusB) opB = (opB * -1).toString();
       } else {
         if (opB === "0") {
           if (btnText === "0") return;
@@ -128,25 +134,61 @@ function processButtonClick(e) {
     }
   } else if (btnText === "=") {
     if ((opA !== undefined) & (opB !== undefined) && operator !== undefined) {
-      if (minusA) opA = -opA;
-      if (minusB) opB = -opB;
       result = performOperation(+opA, operator, +opB);
-      resultText.textContent = result;
-      console.log(result);
+      if (result !== "ERR") {
+        result = result.toFixed(2);
+        resultText.textContent = result;
+      } else {
+        resultText.textContent = "ERR";
+        result = undefined;
+      }
       opA = opB = operator = undefined;
       minusA = minusB = false;
       periodA = periodB = false;
     }
-  } else if (btnText === "AC" || (btnText === "C" && result !== undefined)) {
+  } else if (
+    btnText === "AC" ||
+    (btnText === "C" &&
+      (result !== undefined || resultText.textContent === "ERR"))
+  ) {
     operator = opA = opB = result = undefined;
     processingText.textContent = resultText.textContent = "0";
     minusA = minusB = false;
     periodA = periodB = false;
   } else if (btnText === "C") {
-    const processingString = processingText.textContent;
-    const prStrLen = processingString.length;
+    let processingString = processingText.textContent;
+    const prLen = processingString.length;
+
+    if (processingString[prLen - 1] === ".") {
+      if (opB !== undefined) periodB = false;
+      else periodA = false;
+    }
+
+    if (processingString === "0") return;
 
     processingString = processingString.slice(0, -1);
+    if (processingString.length === 0) {
+      processingText.textContent = "0";
+    } else {
+      if (opB !== undefined) {
+        opB = opB.slice(0, -1);
+        console.log(opB.length);
+        if (opB.length === 0) {
+          minusB = false;
+          opB = undefined;
+        }
+      } else if (operator !== undefined && opB === undefined) {
+        operator = operator.slice(0, -1);
+        if (operator.length === 0) operator = undefined;
+      } else if (opA !== undefined && operator == undefined) {
+        opA = opA.slice(0, -1);
+        if (opA.length === 0) {
+          minusA = false;
+          opA = undefined;
+        }
+      }
+      processingText.textContent = processingString;
+    }
   }
 }
 
